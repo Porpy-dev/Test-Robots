ad094d1bad3678362f39bf89b342ca398389d463
Porpy-dev <114770917+Porpy-dev@users.noreply.github.com>
2024-05-14 18:48:49 +0200

    DATA on update

--- RSK_mesures.py
+++ RSK_mesures.py
@@ -6,42 +6,55 @@ import threading
 import os
 import matplotlib.pyplot as plt
 
-with rsk.Client(host='192.168.1.36', key='123') as client:
+with rsk.Client(host='192.168.1.36', key='') as client:
     cobaye = client.robots['green'][1]
     x_pose_list = []
-    frame_time = 0.05
+    x_time = []
+    recording = True
+    destination = (1, 0, 0)
+    value_number = 100
 
-    def start_test(repetition, dt):
-        count = 0
-        test = True
-        destination = (1, 0, 0)
-        cobaye.goto((-destination[0], destination[1], destination[2]), wait = True)
-        while test:
-            cobaye.goto(destination, wait = False)
-            x_pose_list.append(round(cobaye.pose[0], 4))
-            print(round(cobaye.pose[0], 4))
-            time.sleep(dt)
-            if abs(cobaye.pose[0] - destination[0]) < 0.01:
-                destination = (-destination[0], 0, 0)
-                count += 1
-                if count == repetition:
-                    test = False
+
+    def record_test(Client, dt):
+        if len(x_time) <= value_number:
+            x_pose_list.append(round(cobaye.pose[0], 8))
+            x_time.append(round(dt*len(x_time) + dt, 8))
+            print(round(x_pose_list[len(x_pose_list)-1]-x_pose_list[len(x_pose_list)-2], 4), round(x_time[len(x_time)-1], 4))
+        else:
+            return False
+        
+            
+            
+
         
 
+    cobaye.goto((-1, 0, 0), wait = True)
 
-    start_test(2, frame_time)
+    while recording:
 
+        client.on_update = record_test
+        cobaye.goto((destination), wait = False)
+        if abs(cobaye.pose[0] - destination[0]) < 0.01:
+            destination = (-destination[0], 0, 0)
+        if len(x_time) > value_number:
+            recording = False
+            print('End of recording')
+            print (len(x_time))
+            print (len(x_pose_list))
+            
+        
 
-    x_time = [frame_time * i for i in range(len(x_pose_list))]
     x_speed = abs(np.diff(x_pose_list) / np.diff(x_time))
     print(x_speed)
+
+
     x_acceleration = abs(np.diff(x_speed) / np.diff(x_time[1:]))
 
-
-    print('done')
-    plt.plot(x_time, x_pose_list)
-    plt.plot(x_time[1:], x_speed)
-    plt.plot(x_time[2:], x_acceleration)
+    plt.scatter(x_time, x_pose_list, s=2)
+    plt.show()
+    plt.scatter(x_time[1:], x_speed, s=2)
+    plt.show()
+    plt.scatter(x_time[2:], x_acceleration, s=2)
     plt.show()
 
     
